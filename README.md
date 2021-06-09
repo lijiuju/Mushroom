2e4bf3d0d2408d0ff9bf6db879d8af19fe995dd6
lijiuju <59941265@qq.com>
2021-06-09 10:43:39 +0800

    项目完成

---
+++ README.md
@@ -0,0 +1,28 @@
+# xmgmail
+
+```该项目来自codewhy老师的蘑菇街项目,小白跟着做缝缝补补算完成了```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar class="fix"></main-tab-bar>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from 'content/mainTabbar/MainTabBar'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar,
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+
+  #app {
+    position: relative;
+  }
+  .fix{
+    position: fixed;
+  }
+</style>
